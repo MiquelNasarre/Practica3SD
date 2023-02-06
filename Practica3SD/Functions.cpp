0836bb55860894acdb5ef7fdc2eb1afc576f2be8
Miquel <miguel.nasarre.budino@gmail.com>
2023-02-06 15:12:21 +0100

    Final Update

--- Practica3SD/Functions.cpp
+++ Practica3SD/Functions.cpp
@@ -9,7 +9,7 @@ double* orbit(double x0, double mu) {
     array[1] = first;
     for (int i = 2; i < N; i++) {
         last = f(last, mu);
-        if (last - first < Delta && first - last < Delta) break;
+        if (fabs(last - first) < Delta) break;
         array[i] = last;
     }
     return array;
@@ -28,79 +28,9 @@ double** orbits(double x0) {
     return matrix;
 }
 
-// f^n(x, mu)
 double fn(double x, double mu, int n) {
     for (int i = 0; i < n; i++) x = f(x, mu);
     return x;
-}
-
-// numeric differentiation using symmetric difference formula (order 1)
-double fn_x(double x, double mu, int n) {
-    return (fn(x + h, mu, n) - fn(x - h, mu, n)) / (2 * h);
-}
-
-// numeric differentiation using symmetric difference formula (order 1)
-double fn_mu(double x, double mu, int n) {
-    return (fn(x, mu + h, n) - fn(x, mu - h, n)) / (2 * h);
-}
-
-// numeric differentiation using symmetric difference formula (order 2)
-double fn_xx(double x, double mu, int n) {
-    return (fn(x + h, mu, n) - 2 * fn(x, mu, n) + fn(x - h, mu, n)) / (h * h);
-}
-
-// numeric differentiation for mixed derivatives
-double fn_xmu(double x, double mu, int n) {
-    return (fn(x + h, mu + h, n) - fn(x - h, mu + h, n) - fn(x + h, mu - h, n) + fn(x - h, mu - h, n)) / (4 * h * h);
-}
-
-// f(x, mu) = x
-double Fx_power2(double x, double mu, int n) {
-    return fn(x, mu, n) - x;
-}
-
-// diff(f(x, mu), x) = -1
-double Fy_power2(double x, double mu, int n) {
-    return fn_x(x, mu, n) + 1;
-}
-
-// f(x, mu) = x
-double Fx(double x, double mu, int n) {
-    return fn(x, mu, n) - x;
-}
-
-// diff(f(x, mu), x) = 1
-double Fy(double x, double mu, int n) {
-    return fn_x(x, mu, n) - 1;
-}
-
-double* newton(double F(double, double, int), double dF(double, double, int),double x0, double mu0, int n) {
-    const int MAX_ITER = 100;
-    int iter = 0;
-    double x = x0, mu = mu0;
-    double x_aux, mu_aux;
-    double d, deval;
-    double d11, d12, d21, d22;  // differential matrix
-    double det;
-    // functions of newton: F = (Fx, Fy).
-    do {
-        d11 = fn_x(x, mu, n);
-        d12 = fn_mu(x, mu, n);
-        d21 = fn_xx(x, mu, n);
-        d22 = fn_xmu(x, mu, n);
-        det = d11 * d22 - d12 * d21;
-        x_aux = x - (d22 * F(x, mu, n) - d12 * dF(x, mu, n)) / det;
-        mu_aux = mu - (-d21 * F(x, mu, n) + d11 * dF(x, mu, n)) / det;
-        d = MAX(fabs(x - x_aux), fabs(mu - mu_aux));
-        x = x_aux;
-        mu = mu_aux;
-        deval = MAX(fabs(F(x, mu, n)), fabs(dF(x, mu, n)));
-        iter++;
-    } while (iter < MAX_ITER && MAX(d, deval) > EPS);
-    double* v = (double*)malloc(2 * sizeof(double));
-    v[0] = x;
-    v[1] = mu;
-    return v;
 }
 
 /* ROUTINES */
@@ -161,20 +91,22 @@ void MuFinder() {
 
 void OddPeriodOrbits() {
     const int MaxPer = 11, Iteracions = 5000;
-    const double Step = 0.000002;
+    const double Step = 0.000001, CloserStep = 0.000000001;
     int i = MaxPer;
     double mu = MuInit;
     while (i >= 3) {
-        while (mu < EOInterval) {
-            double s = 0.7;
+        double s = 0.7;
+        while (mu < EOInterval && fabs(s - fn(s, mu, i)) > EPS) {
             mu += Step;
             for (int j = 0; j < Iteracions; j++) s = f(s, mu);
-
-            if (fabs(s - fn(s,mu,i)) < EPS) {
-                printf("PERIODE %i: %c = %lf\n", i, 230, *(newton(Fx, Fy, s, mu, i) + 1));
-                break;
-            }
         }
+        mu -= Step;
+        for (int j = 0; j < Iteracions; j++) s = f(s, mu);
+        while (mu < EOInterval && fabs(s - fn(s, mu, i)) > EPS) {
+            mu += CloserStep;
+            for (int j = 0; j < Iteracions; j++) s = f(s, mu);
+        }
+        printf("PERIODE %i: %c = %.8f\n", i, 230, mu);
         i -= 2;
     }
 }
